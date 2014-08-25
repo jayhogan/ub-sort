f9607087a80b55229e329f2fa4a913b9785a601c
Hogan, Jay <jay.hogan@genworth.com>
2014-08-25 05:52:22 -0400

    Switch to using browserify API instead of gulp-browserify; ugligy

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 var gulp = require('gulp');
-var browserify = require('gulp-browserify');
+var browserify = require('browserify');
+var uglify = require('gulp-uglify');
+var source = require('vinyl-source-stream');
 var rename = require('gulp-rename');
 
 gulp.task('default', ['build']);
+gulp.task('build', ['browserify', 'uglify']);
 
-gulp.task('build', function() {
-  gulp.src('modules/index.js')
-    .pipe(browserify({
+gulp.task('browserify', function() {
+  // TODO - Add file header
+  return browserify({
+      entries: './modules/index.js',
       standalone: 'ng-utility-belt',
       debug: true
-    }))
-    .pipe(rename('ng-utility-belt.js'))
-    .pipe(gulp.dest('./dist'))
+    }).bundle()
+    .pipe(source('ng-utility-belt.js'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('uglify', ['browserify'], function() {
+  // TODO - Preserve file header comment (when added)
+  return gulp.src('dist/ng-utility-belt.js')
+    .pipe(uglify())
+    .pipe(rename('ng-utility-belt.min.js'))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('test', function() {
